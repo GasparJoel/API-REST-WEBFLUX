ff6ad26532c55f6ce741293225cf2b235df45906
Joel Gt <0332201030@unjfsc.edu.pe>
2025-04-28 12:33:23 -0500

    Integre las pruebas Junit en el metohodo Get , Ok

--- src/main/java/com/joekl/demowebflux_2025/controller/TodoController.java
+++ src/main/java/com/joekl/demowebflux_2025/controller/TodoController.java
@@ -4,30 +4,36 @@ import com.joekl.demowebflux_2025.model.Todo;
 import com.joekl.demowebflux_2025.repository.TodoRepository;
 import com.joekl.demowebflux_2025.service.TodoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.net.URI;
 import java.time.Duration;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/todos")
-public class TodoController     {
-
-    private final TodoService todoService;
-
-
-    @GetMapping
-    public Flux<Todo> getAll(){
-
-        return todoService.findAll()
-                .delayElements(Duration.ofSeconds(2));
-    }
+    public class TodoController     {
+    
+        private final TodoService todoService;
+    
+    
+        @GetMapping
+        public Flux<Todo> getAll(){
+    
+            return todoService.findAll();
+    //                .delayElements(Duration.ofSeconds(2));
+        }
 
     @GetMapping("/{id}")
-    public Mono<Todo> get(@PathVariable String id){
-        return todoService.findById(id);
+    public Mono<ResponseEntity<Todo>> get(@PathVariable String id){
+        return  todoService.findById(id)
+                .map(todo -> ResponseEntity.ok(todo))
+                .switchIfEmpty(Mono.just(ResponseEntity.notFound().build()));
+
+        //return todoService.findById(id);
     }
 
     @PostMapping
---
+++ src/test/java/com/joekl/demowebflux_2025/TodoTestController.java
@@ -0,0 +1,68 @@
+package com.joekl.demowebflux_2025;
+
+import com.joekl.demowebflux_2025.model.Todo;
+import com.joekl.demowebflux_2025.repository.TodoRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class TodoTestController {
+
+    @Autowired
+    //Esto viene de reactor
+    private WebTestClient webTestClient;
+
+    //
+    @Autowired
+    private TodoRepository todoRepository;
+//    pueba N°1
+    @Test
+    void getAll() {
+        webTestClient.get()
+                .uri("/api/v1/todos")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(Todo.class)
+                .consumeWith(result -> {
+                    var todos = result.getResponseBody();
+                    Assertions.assertThat(todos).isNotEmpty();   // No está vacío
+                    Assertions.assertThat(todos.get(0).getId()).isNotNull(); // El primer id no es nulo
+                });
+    }
+
+    @Test
+    void getNotFound() {
+
+        webTestClient.get()
+                .uri("api/v1/todos/{id}","id no existe")
+                .exchange()
+                .expectStatus().isNotFound()
+                .expectBody().isEmpty();
+    }
+    @Test
+    void get(){
+        Todo todo = createExampleTodo();
+        webTestClient.get()
+                .uri("api/v1/todos/{id}",todo.getId())
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.name").isEqualTo(todo.getName())
+                .jsonPath("$.completed").isEqualTo(todo.getCompleted())
+                .jsonPath("$.id").isEqualTo(todo.getId());
+
+    }
+
+    private Todo createExampleTodo(){
+        Todo todo = new Todo();
+        todo.setName("Repasar la clase");
+        todo.setCompleted(false);
+
+      return   todoRepository.save(todo).block();
+
+    }
+
+}
